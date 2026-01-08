8306a0ad6eb9d2d2a0a6779b616f5aa6d7e649ac
oliulalam <mdoliulalamjoy@gmail.com>
2026-01-08 20:28:19 +0600

    Third page first layout design done

--- lib/third_page.dart
+++ lib/third_page.dart
@@ -7,98 +7,280 @@ class ThaedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFFBCE8AE),
-        title: Text("Recipes"),
-        actions: [
-          Container(
-            height: 45,
-            width: 45,
-            decoration: BoxDecoration(
-              color: Color(0xFF93D877),
-              borderRadius: BorderRadius.circular(25),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFFBCE8AE),
+          title: Text("Recipes"),
+          actions: [
+            Container(
+              height: 45,
+              width: 45,
+              decoration: BoxDecoration(
+                color: Color(0xFF93D877),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Center(child: Icon(Icons.search, size: 30)),
             ),
-            child: Center(child: Icon(Icons.search, size: 30)),
-          ),
-          SizedBox(width: 10),
-          Container(
-            height: 45,
-            width: 45,
-            decoration: BoxDecoration(
-              color: Color(0xFF93D877),
-              borderRadius: BorderRadius.circular(25),
+            SizedBox(width: 10),
+            Container(
+              height: 45,
+              width: 45,
+              decoration: BoxDecoration(
+                color: Color(0xFF93D877),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Center(
+                child: Icon(Icons.notifications_none_outlined, size: 30),
+              ),
             ),
-            child: Center(
-              child: Icon(Icons.notifications_none_outlined, size: 30),
+            SizedBox(width: 10),
+            Container(
+              height: 45,
+              width: 45,
+              decoration: BoxDecoration(
+                color: Color(0xFF93D877),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Center(child: Icon(Icons.bookmark_add_outlined, size: 30)),
             ),
-          ),
-          SizedBox(width: 10),
-          Container(
-            height: 45,
-            width: 45,
-            decoration: BoxDecoration(
-              color: Color(0xFF93D877),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Center(child: Icon(Icons.bookmark_add_outlined, size: 30)),
-          ),
 
-          SizedBox(width: 20),
-        ],
-      ),
+            SizedBox(width: 20),
+          ],
+        ),
 
-      body: Column(
-        children: [
-          SizedBox(height: 10),
+        body: Column(
+          children: [
+            SizedBox(height: 10),
 
-          SizedBox(
-            height: 260,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              shrinkWrap: true,
-              itemCount: 10,
-              itemBuilder: (context, index){
-                return Container(
-                  margin: EdgeInsets.only(left: 15,),
-                  height: 250,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFEBDDDCFF),
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        blurRadius: 10,
-                        offset: Offset(0, 6),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30),
-                            topRight: Radius.circular(30),
-                          ),
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/one.jpeg'),
-                            fit: BoxFit.cover,
+            SizedBox(
+              height: 230,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(left: 15),
+                    height: 180,
+                    width: 170,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFEBDDDCFF),
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 10,
+                          offset: Offset(0, 6),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 180,
+                          width: 170,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30),
+                            ),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/one.jpeg'),
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 10,),
-                      Text("Vegan", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600, fontFamily: 'Roboto'),),
-                    ],
-                  ),
-                );
-              },
+                        SizedBox(height: 10),
+                        Text(
+                          "Vegan",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Roboto',
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
-          )
-        ],
+            SizedBox(height: 20),
+            TabBar(
+              tabs: [
+                Text("My preferences", style: TextStyle(fontSize: 18)),
+                Text("All", style: TextStyle(fontSize: 18)),
+              ],
+            ),
+
+            Expanded(
+              child: TabBarView(
+                children: [
+                  // First Layout
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 15.0,
+                      right: 15.0,
+                      top: 15.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      children: [
+                        Text(
+                          "Breakfast",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+
+                        SizedBox(height: 10),
+
+                        Container(
+                          height: 250,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFEBDDDCFF),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            children: [
+                              Stack(
+                                children: [
+                                  Container(
+                                    height: 160,
+                                    width: 200,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: AssetImage(
+                                          'assets/images/ten.jpeg',
+                                        ),
+                                        fit: BoxFit.cover,
+                                      ),
+                                      borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(20),
+                                        topLeft: Radius.circular(20),
+                                      ),
+                                    ),
+                                  ),
+
+                                  Positioned(
+                                    bottom: 0,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(10.0),
+                                      child: Row(
+                                        children: [
+                                          Container(
+                                            height: 40,
+                                            width: 40,
+                                            decoration: BoxDecoration(
+                                              color: Colors.white,
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                            ),
+                                            child: Icon(Icons.menu_book),
+                                          ),
+                                          SizedBox(width: 5),
+                                          Container(
+                                            height: 35,
+                                            width: 60,
+                                            decoration: BoxDecoration(
+                                              color: Colors.black45,
+                                              borderRadius:
+                                                  BorderRadius.circular(12),
+                                            ),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                Icon(
+                                                  Icons.ramen_dining,
+                                                  color: Colors.greenAccent,
+                                                ),
+                                                Text(
+                                                  '5',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 18,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          SizedBox(width: 15),
+                                          Container(
+                                            height: 50,
+                                            width: 50,
+                                            decoration: BoxDecoration(
+                                              color: Colors.black45,
+                                              borderRadius:
+                                                  BorderRadius.circular(25),
+                                            ),
+                                            child: Icon(
+                                              Icons.bookmark_add_outlined,
+                                              color: Colors.white,
+                                              size: 30,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              Column(
+                                children: [
+                                  Text(
+                                    "Salad with tomatoes",
+                                    style: TextStyle(
+                                      fontSize: 25,
+                                      height: 0.9,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.start,
+                                  ),
+
+                                  Container(
+                                    height: 30,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                      color: Colors.amberAccent.shade100,
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.local_fire_department,
+                                          color: Colors.amber.shade900,
+                                        ),
+                                        Text(
+                                          '194 kcal',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w800,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ), // main container
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
